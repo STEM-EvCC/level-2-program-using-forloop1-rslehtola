68bec5380c5042ee3f5bffc809a3341dae930646
rslehtola <rslehtola@students.everettcc.edu>
2025-05-14 23:30:15 +0000

    Setting up main.py

--- main.py
+++ main.py
@@ -1,3 +1,27 @@
+# Programming with For Loop
+# Coder: Rory Lehtola
+# Date: 05/14/25
+# Class: STEM 103 Spring
+
+# Notes:
+
+#
+
+# What I learned:
+
+#
+
+# Assignment:
+
 mission_names = ['Apollo 11', 'Challenger', 'Curiosity Rover', 'Viking 1', 'Mars Pathfinder', 'Hubble Telescope', 'Apollo 13']
 mission_years = [1969, 1986, 2012, 1975, 1996, 1990, 1970]
 mission_success = [True, False, True, True, True, True, False]
+
+
+
+
+
+
+
+
+
