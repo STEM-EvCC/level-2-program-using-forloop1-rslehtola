97cbe85ea53b791dcebad083c84916e006ee7955
rslehtola <rslehtola@students.everettcc.edu>
2025-05-15 01:32:39 +0000

    coding on main.py

--- main.py
+++ main.py
@@ -17,13 +17,28 @@ mission_names = ['Apollo 11', 'Challenger', 'Curiosity Rover', 'Viking 1', 'Mars
 mission_years = [1969, 1986, 2012, 1975, 1996, 1990, 1970]
 mission_success = [True, False, True, True, True, True, False]
 
-# variable = 
-# print(f"This is the value{variable:.2f}%")
+# Count the total number of space missions and mission success's
+total_missions = len(mission_names)
+total_success = sum(mission_success)
 
-# Counts the total number of space missions.
-# Counts the number of successful missions.
-# Calculates the success rate of the missions.
-# Lists all the missions that were launched before the year 2000.
+# Calculate the success rate of missions
+success_rate = (total_success / total_missions) * 100
+
+# Lists all the missions that were launched before the year 2000
+missions_pre2000 = [mission_names[i] for i in range (total_missions) if mission_years[i] < 2000]
+
+# Print the total number of missions and successful missions
+print("Total number of space missions: ", total_missions)
+print("Total number of successful space missions: ", total_success)
+
+# Print success rate of missions
+print(f"This is the value of {success_rate:.2f}%")
+
+# Print the missions launched before 2000
+print("Missions launched pre 2000: ")
+
+for mission in missions_pre2000:
+    print(mission)
 
 # Use a for loop to iterate through the list of missions.
 # Count the total number of missions.
